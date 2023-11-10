59ba6969a5f1b7a45467a132bfc1a4f96fc28ebe
mukmukmukmuk <ksm010825@naver.com>
2023-11-10 17:44:58 +0900

    asdf

--- Apiprj/settings.py
+++ Apiprj/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os
 import dj_database_url
 import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,7 +19,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -74,7 +76,7 @@ WSGI_APPLICATION = "Apiprj.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
