866be1ecf7b93ca61e0d31c05b45d9b18e2dd559
mukmukmukmuk <ksm010825@naver.com>
2023-11-10 21:31:37 +0900

    adsf

--- Apiprj/settings.py
+++ Apiprj/settings.py
@@ -72,15 +72,19 @@ WSGI_APPLICATION = "Apiprj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#    }
+# }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+DATABASES['default'].update(dj_database_url.config(
+    conn_max_age=500, ssl_require=True))
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
